5885cbccbf6126b9eae7e8fd75fd03eff7f983ae
david <davidpark1208@gmail.com>
2016-07-07 00:51:18 +0900

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705175828) do
+ActiveRecord::Schema.define(version: 20160705181049) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20160705175828) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bulletin_id"
+    t.index ["bulletin_id"], name: "index_posts_on_bulletin_id"
   end
 
 end
